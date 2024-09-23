5e7f6af4e6a487981dadab448027055789c57454
KarolisG <kgaivenis@gmail.com>
2024-09-23 16:09:01 +0300

    chore: init app funcionality when user enters the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { socket } from '@/libs/socket'
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected)
+  const [nickName, setNickName] = useState('')
+
+  useEffect(() => {
+    socket.connect()
+
+    socket.on('connect', () => setIsConnected(true))
+    socket.on('disconnect', () => setIsConnected(false))
+
+    return () => {
+      socket.off('connect')
+      socket.off('disconnect')
+    }
+  }, [])
+
+  function handleNicknameSubmit(event) {
+    event.preventDefault()
+    console.log(nickName)
+  }
 
   return (
     <div>
+      <form onSubmit={handleNicknameSubmit}>
+        <label htmlFor="nickName">Enter your nickname:</label>
+        <input
+          id="nickName"
+          type="text"
+          value={nickName}
+          onChange={e => setNickName(e.target.value)}
+        />
+      </form>
+
       <p>Connected: {isConnected ? 'Yes' : 'No'}</p>
     </div>
   )
