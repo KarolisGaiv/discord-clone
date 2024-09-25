62e62e9096262bf37c0f4d2107728c0324e658fc
KarolisG <kgaivenis@gmail.com>
2024-09-25 20:58:56 +0300

    style: add icons to input field

--- src/components/Chat/ChatWindow.jsx
+++ src/components/Chat/ChatWindow.jsx
@@ -3,6 +3,13 @@ import { useState } from 'react'
 import Avatar from 'boring-avatars'
 import moment from 'moment'
 import './style.css'
+import { AiTwotonePlusCircle } from 'react-icons/ai'
+import { GoGift } from 'react-icons/go'
+import { PiGif } from 'react-icons/pi'
+import { TbFileSmile } from 'react-icons/tb'
+import { FaRegFaceSmile } from 'react-icons/fa6'
+import { FaIcons } from 'react-icons/fa'
+// import { FaIcons } from "react-icons/fa6";
 
 export default function ChatWindow({ channel, messages }) {
   const [newMessage, setNewMessage] = useState('')
@@ -39,12 +46,30 @@ export default function ChatWindow({ channel, messages }) {
       </div>
 
       <form className="text-field-wrapper" onSubmit={handleSendMessage}>
+        <AiTwotonePlusCircle className="icon" />
         <input
           type="text"
           value={newMessage}
           onChange={e => setNewMessage(e.target.value)}
           placeholder={`Message #${channel.name}`}
         />
+        <div className="right-side-icons-wrapper">
+          <div className="icon-wrapper">
+            <GoGift className="icon" />
+          </div>
+
+          <div className="gif-icon-wrapper">
+            <PiGif className="icon" />
+          </div>
+
+          <div className="icon-wrapper">
+            <TbFileSmile className="icon" />
+          </div>
+
+          <div className="icon-wrapper">
+            <FaRegFaceSmile className="icon" />
+          </div>
+        </div>
       </form>
     </main>
   )
